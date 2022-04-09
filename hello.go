853e00a2b04b27335d7b3631102ab8daefd4a503
Ícaro <ias_epf@hotmail.com>
2022-04-09 10:51:01 -0300

    "Função de monitoramento"

--- hello.go
+++ hello.go
@@ -2,29 +2,35 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+	"time"
 )
+
+const monitoramentos = 3
+const delay = 5
 
 func main() {
 
 	showInitial()
-	showMenu()
 
-	comando := readyCmd()
+	for {
+		showMenu()
+		comando := readyCmd()
 
-	switch comando {
-	case 1:
-		fmt.Println("Monitorando...")
-	case 2:
-		fmt.Println("Exibindo logs...")
-	case 0:
-		fmt.Println("Saindo do programa...")
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço este comando")
-		os.Exit(-1)
+		switch comando {
+		case 1:
+			startMonitor()
+		case 2:
+			fmt.Println("Exibindo logs...")
+		case 0:
+			fmt.Println("Saindo do programa...")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}
 	}
-
 }
 
 func showInitial() {
@@ -47,4 +53,34 @@ func readyCmd() int {
 	fmt.Println("O valor do comando é: ", cmd)
 
 	return cmd
+}
+
+func startMonitor() {
+	fmt.Println("Monitorando...")
+
+	sites := []string{"https://random-status-code.herokuapp.com/",
+		"https://www.alura.com.br", "https://www.caelum.com.br"}
+
+	for i := 0; i < monitoramentos; i++ {
+		for i, v := range sites {
+			fmt.Println("Testando site", i, ":", v)
+
+			testSites(v)
+		}
+		time.Sleep(delay * time.Second)
+
+		fmt.Println("")
+	}
+	fmt.Println("")
+
+}
+
+func testSites(site string) {
+	resp, _ := http.Get(site)
+
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, "foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, "está com problemas. Status Code:", resp.StatusCode)
+	}
 }
