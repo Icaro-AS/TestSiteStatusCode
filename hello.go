4845f59327551603c9b72d5de1bfbb15c3cf362e
Ícaro <ias_epf@hotmail.com>
2022-04-09 08:42:47 -0300

    Separando funcionalidades

--- hello.go
+++ hello.go
@@ -1,21 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+
+	showInitial()
+	showMenu()
+
+	comando := readyCmd()
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo logs...")
+	case 0:
+		fmt.Println("Saindo do programa...")
+		os.Exit(0)
+	default:
+		fmt.Println("Não conheço este comando")
+		os.Exit(-1)
+	}
+
+}
+
+func showInitial() {
 	nome := "Ícaro"
 	versao := 1.1
-
 	fmt.Println("Olá senhor, ", nome)
 	fmt.Println("Versão do programa: ", versao)
+}
 
+func showMenu() {
 	fmt.Println("1- Iniciar Monitoramento")
 	fmt.Println("2- Exibir Logs")
 	fmt.Println("0- Sair do Programa")
+}
 
-	var comando int
-	fmt.Scan(&comando)
+func readyCmd() int {
+	var cmd int
+	fmt.Scan(&cmd)
 
-	fmt.Println("O valor do comando é: ", comando)
+	fmt.Println("O valor do comando é: ", cmd)
 
+	return cmd
 }
