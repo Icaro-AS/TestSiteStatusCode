eda8c4328c55b50fbfda1ca67618dd40af6e4746
Ícaro <ias_epf@hotmail.com>
2022-04-09 07:24:17 -0300

    menu do programa

--- hello.go
+++ hello.go
@@ -3,5 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world com go")
+	nome := "Ícaro"
+	versao := 1.1
+
+	fmt.Println("Olá senhor, ", nome)
+	fmt.Println("Versão do programa: ", versao)
+
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+
+	var comando int
+	fmt.Scan(&comando)
+
+	fmt.Println("O valor do comando é: ", comando)
+
 }
